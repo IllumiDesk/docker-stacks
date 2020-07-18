e64e9a4c8fbe0145d77f4a1bc479e92b590b7a15
Greg Werner <werner.greg@gmail.com>
2020-07-17 22:56:26 -0400

    feat: Install Julia packages in base image with install.jl file (#6)

---
+++ base-notebook/install.jl
@@ -0,0 +1,24 @@
+# This mechanism allows us to import a package list.
+# Source: https://discourse.julialang.org/t/building-a-dockerfile-with-packages/37272/2
+
+using Pkg
+pkg"add Colors"
+pkg"add CSV"
+pkg"add GR"
+pkg"add IJulia"
+pkg"add ImageView"
+pkg"add ImageInTerminal"
+pkg"add FileIO"
+pkg"add ImageFiltering"
+pkg"add Images"
+pkg"add ImageFeatures"
+pkg"add LinearAlgebra"
+pkg"add Plots"
+pkg"add PyPlot"
+pkg"add Random"
+pkg"add GtkReactive"
+pkg"add Roots"
+pkg"add SymEngine"
+pkg"add Statistics"
+pkg"add TestImages"
+pkg"precompile"
