516142952bcd2c4a45fab4497c8430ab87eded1e
Greg <werner.greg@gmail.com>
2020-08-05 15:18:30 -0400

    add grader role to script, remove learner and instructor images
    Signed-off-by: Greg <werner.greg@gmail.com>

--- base-notebook/update-extensions.sh
+++ base-notebook/update-extensions.sh
@@ -13,7 +13,13 @@ install_student_extensions() (
     jupyter nbextension enable --sys-prefix assignment_list/main --section=tree
 )
 
-if [[ "${USER_ROLE}" == "Instructor" ]]|| [[ "${USER_ROLE}" == "TeachingAssistant" ]]; then
+install_grader_extensions() (
+    jupyter nbextension enable --sys-prefix create_assignment/main
+    jupyter nbextension enable --sys-prefix formgrader/main --section=tree
+    jupyter serverextension enable --sys-prefix nbgrader.server_extensions.formgrader
+)
+
+if [[ "${USER_ROLE}" == "Instructor" ]] || [[ "${USER_ROLE}" == "TeachingAssistant" ]]; then
     echo "Enabling nbgrader extensions for Instructor or TeachingAssistant role"
     install_instructor_extensions
 fi
@@ -22,3 +28,9 @@ if [[ "${USER_ROLE}" == "Student" ]] || [[ "${USER_ROLE}" == "Learner" ]]; then
     echo "Enabling nbgrader extensions for Student/Learner role"
     install_student_extensions
 fi
+
+if [[ "${USER_ROLE}" == "Grader" ]]; then
+    echo "Enabling nbgrader extensions for Grader role"
+    install_grader_extensions
+fi
+
