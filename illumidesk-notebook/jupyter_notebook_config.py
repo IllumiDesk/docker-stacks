6f1a6f0c334d92ac18045a8ce786e49b68ae32c0
Greg <werner.greg@gmail.com>
2020-08-13 20:47:50 -0400

    add custom jupyter config to support iframes
    Signed-off-by: Greg <werner.greg@gmail.com>

--- illumidesk-notebook/jupyter_notebook_config.py
+++ illumidesk-notebook/jupyter_notebook_config.py
@@ -4,4 +4,8 @@ c = get_config()
 # load base config
 load_subconfig('/etc/jupyter/jupyter_notebook_config_base.py')
 
+c.NotebookApp.tornado_settings = {"headers": {"Content-Security-Policy": "frame-ancestors 'self' *"}}
+c.NotebookApp.allow_root = True
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.token = ''
 c.NotebookApp.default_url = '/tree'
