70647f30aac1d9edae55612c56d21edff2d850b1
Greg Werner <werner.greg@gmail.com>
2020-10-23 17:59:44 -0400

    move conditionals to bash script
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

---
+++ scripts/after_success.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+dockerhub_login () {
+    echo "${DOCKER_TOKEN}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+}
+
+# no need to build and tag images for pull requests.
+tag_and_push () {
+  if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+    if [ "${TRAVIS_BRANCH}" == "main" ]; then
+      export TAG="${TRAVIS_BUILD_NUMBER}"
+      make build-all
+    fi
+  fi
+}
+
+main () {
+  echo "DockerHub login..."
+  dockerhub_login
+  echo "Build and push images..."
+  tag_and_push
+}
+
+main
