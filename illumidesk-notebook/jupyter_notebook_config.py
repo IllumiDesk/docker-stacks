6f79a0b57b57dea726feb5679039e6340e02c53f
Greg Werner <werner.greg@gmail.com>
2020-11-11 06:41:50 -0500

    add julia to base image and revert to python3.7
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

--- illumidesk-notebook/jupyter_notebook_config.py
+++ illumidesk-notebook/jupyter_notebook_config.py
@@ -4,6 +4,14 @@ c = get_config()
 # load base config
 load_subconfig('/etc/jupyter/jupyter_notebook_config_base.py')
 
+# monkeypatch for python3.7
+try:
+    from http.cookies import Morsel
+except ImportError:
+    from Cookie import Morsel
+
+Morsel._reserved[str('samesite')] = str('SameSite')
+
 # supports iframe and samesite cookies
 c.NotebookApp.tornado_settings = {
     "headers": {"Content-Security-Policy": "frame-ancestors 'self' *"},
