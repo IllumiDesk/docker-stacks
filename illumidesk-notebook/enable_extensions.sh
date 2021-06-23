44cebd29b8e9b7929201f36e70e617ba2184367b
Rupesh Parab <rupeshparab.rp@gmail.com>
2021-06-23 23:27:30 +0530

    chore: Add async_nbgrader package to docker image (#44)

--- illumidesk-notebook/enable_extensions.sh
+++ illumidesk-notebook/enable_extensions.sh
@@ -3,8 +3,10 @@
 install_instructor_extensions() (
     jupyter serverextension enable --sys-prefix nbgrader.server_extensions.assignment_list
     jupyter serverextension enable --sys-prefix nbgrader.server_extensions.course_list
+    jupyter serverextension enable --sys-prefix async_nbgrader
     jupyter nbextension enable --sys-prefix course_list/main --section=tree
     jupyter nbextension enable --sys-prefix assignment_list/main --section=tree
+    jupyter nbextension enable --sys-prefix --py async_nbgrader/common
 )
 
 install_student_extensions() (
