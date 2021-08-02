d3edf0caf3baaf42ae9feb04abb98abc5e614087
Greg Werner <werner.greg@gmail.com>
2021-08-02 09:36:16 -0400

    update requirements and nbgrader configs
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

--- illumidesk-notebook/global_nbgrader_config.py
+++ illumidesk-notebook/global_nbgrader_config.py
@@ -3,8 +3,19 @@ from nbgrader.auth import JupyterHubAuthPlugin
 
 c = get_config()
 
+# Basic logging enabled
+c.Application.log_level = 10
 
-c.Exchange.path_includes_course = True
-c.Exchange.root = "/srv/nbgrader/exchange"
+# Use the JupyterHub for authentication and group membership
 c.Authenticator.plugin_class = JupyterHubAuthPlugin
-c.Application.log_level = 30
+
+# Used when managing more than one course
+c.Exchange.path_includes_course = True
+
+# Exchange root path. If using a container, mount this folder
+# for persistence.
+c.Exchange.root = "/srv/nbgrader/exchange"
+
+# Increase timeouts to avoid unnecessary exits when autograding
+c.ExecutePreprocessor.iopub_timeout=180
+c.ExecutePreprocessor.timeout=360
--- illumidesk-notebook/test/test_languages.py
+++ illumidesk-notebook/test/test_languages.py
@@ -8,17 +8,33 @@ import pytest
 
 LOGGER = logging.getLogger(__name__)
 
+PYTHON_VERSION='3.8.8'
+NOTEBOOK_IMAGE_TAG=f'python-{PYTHON_VERSION}'
+NOTEBOOK_IMAGE_NAME= 'illumidesk/illumidesk-notebook'
+NOTEBOOK_IMAGE=f'{NOTEBOOK_IMAGE_NAME}:{NOTEBOOK_IMAGE_TAG}'
 
 @pytest.mark.parametrize(
-    'language,version_output', [('python', ['Python', '3.9.5\n']),],
+    'language,version_output',
+    [
+        ('python', ['Python', f'{PYTHON_VERSION}\n']),
+    ],
 )
 def test_languages(language, version_output):
     """Ensure that the language is available in the container's PATH and that
     it has the correct version
     """
-    LOGGER.info(f'Test that language {language} is correctly installed ...')
+    LOGGER.info(f'Test that language {language} {PYTHON_VERSION} is correctly installed ...')
     client = docker.from_env()
-    output = client.containers.run('illumidesk/illumidesk-notebook:python-3.9.5', f'{language} --version')
+    output = client.containers.run(NOTEBOOK_IMAGE, f'{language} --version')
     output_decoded = output.decode('utf-8').split(' ')
     assert output_decoded[0:3] == version_output
     LOGGER.info(f'Output from command: {output_decoded[0:3]}')
+
+
+def test_invalid_cmd():
+    """Ensure that an invalid command returns a docker.errors.ContainerError
+    """
+    with pytest.raises(ContainerError):
+        LOGGER.info('Test an invalid command ...')
+        client = docker.from_env()
+        client.containers.run(NOTEBOOK_IMAGE, 'foo --version')
